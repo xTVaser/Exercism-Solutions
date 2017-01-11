90cda5b74dc53e3f2d481b579e4e0f7f915f6a6e
Tyler Wilding <xtvaser@gmail.com>
2017-01-11 15:17:10 -0500

    Finished Run-Length Encoding

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -1,12 +1,14 @@
 module RunLength (decode, encode) where
 
+import Data.Char
+
 encode :: String -> String
 encode []  = ""
 encode str =
     if encodingLength > 1
-       then show encodingLength ++ head str : decode (drop encodingLength str)
+       then show encodingLength ++ head str : encode (drop encodingLength str)
     else
-        head str : decode (drop encodingLength str)
+        head str : encode (drop encodingLength str)
     where encodingLength = length (takeWhile (== head str) str)
 
 -- With replicate list2 list1
@@ -15,3 +17,8 @@ encode str =
 decode :: String -> String
 decode [] = ""
 decode str =
+    if isNumber (head str) == False
+       then head str : decode (tail str)
+    else
+        replicate (read (takeWhile (isNumber) str)) (head strNumRemoved) ++ decode (tail strNumRemoved)
+            where strNumRemoved = dropWhile (isNumber) str
